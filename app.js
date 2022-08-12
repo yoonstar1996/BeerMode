ab0f94b7192515ff36db8322fa1aef7ff8093b06
slowt <slowt0425@gmail.com>
2022-08-12 18:55:49 +0900

    starting code

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+app.set("view engine", "ejs");
+app.use( '/static', express.static( 'static'));
+const port = 8000;
+
+app.get("/", (req,res)=>{
+    res.render('main');
+})
+
+app.listen(port, ()=>{
+    console.log("server open: ", port);
+})
