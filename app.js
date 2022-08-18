8cf50e575a07751f8db2a7510eb048b13f675837
CHOIALTH <dnjswns4545@gmail.com>
2022-08-18 18:49:39 +0900

    project: adding modal/swal/alert

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get("/", (req,res)=>{
 app.get("/eventCalendar", (req,res)=>{
   res.render("eventCalendar");
 })
+// 캘린더제작할때까지 사용
+app.get("/eventCalendar1", (req,res)=>{
+  res.render("calendar1");
+}) 
 
 app.listen(port, ()=>{
   console.log("server open: ", port);
