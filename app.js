2254f66047b50a80c83f29853801486f5a0ec6c4
CHOIALTH <dnjswns4545@gmail.com>
2022-08-19 16:18:18 +0900

    project: cal with main layout

--- app.js
+++ app.js
@@ -7,11 +7,7 @@ const port = 8000;
 app.get("/", (req,res)=>{
   res.render('main');
 })
-app.get("/eventCalendar", (req,res)=>{
-  res.render("eventCalendar");
-})
-// 캘린더제작할때까지 사용
-app.get("/eventCalendar1", (req,res)=>{
+app.get("/calendar", (req,res)=>{
   res.render("calendar1");
 }) 
 
