8a78dfba0bdd77711139d713c5deb7111cf5fbb3
slowt <slowt0425@gmail.com>
2022-08-19 17:24:19 +0900

    reset

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 const express = require("express");
 const app = express();
 app.set("view engine", "ejs");
-app.use( '/static', express.static( 'static'));
+app.use('/static', express.static('static'));
 const port = 8000;
 
-app.get("/", (req,res)=>{
+app.get("/", (req, res) => {
   res.render('main');
 })
-app.get("/calendar", (req,res)=>{
-  res.render("calendar1");
-}) 
 
-app.listen(port, ()=>{
+app.get("/main", (req, res) => {
+  res.render('main');
+})
+
+app.get("/info", (req, res) => {
+  res.render('inf2');
+})
+
+app.get("/main2", (req, res) => {
+  res.render('main2');
+})
+
+app.get("/mypage", (req, res) => {
+  res.render("mypage");
+})
+
+app.listen(port, () => {
   console.log("server open: ", port);
 })
